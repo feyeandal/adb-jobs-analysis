59811025d6da874c82d87828c90c4bb5244edc6a
Kasun Amarasinghe <amarekas@gmail.com>
2022-06-23 23:25:50 -0400

    skeleton

---
+++ pipeline_skeletons/text_extractor.py
@@ -0,0 +1,30 @@
+
+
+def extract_text_from_image(image):
+    """given an image return the text of the image"""
+    pass
+
+
+def clean_text(text):
+    """given the raw text extracted from an image, 
+    clean the text to prep for evaluation"""
+    pass
+
+
+def calculate_ocr_accuracy(text, acc_threshold):
+    """given the extracted text from an image calculate accuracy"""
+
+    # we could use this function to determine whether the image passes the test
+
+    pass
+
+
+def preprocess_image(image):
+    """given an image that failed the test, do preprocessing"""
+
+    # This could be a more complicated process than one function
+    pass
+
+
+def run_module(images):
+    """run the complete module for a given set of images and return the text"""
