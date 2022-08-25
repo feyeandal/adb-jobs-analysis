71728738ce38633ecf1cd6ecefe0dd05463743b8
Merl Syrex Chandana <merl.chandana@gmail.com>
2022-08-26 00:46:45 +0530

    made changes to lda topic modeling

--- src/skills_analysis.py
+++ src/skills_analysis.py
@@ -15,16 +15,20 @@ file_path = "E:/ADB_Project/code/data/pipeline_sample.csv"
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 
 def sent_to_words(sentences):
+    """Lowercases and converts each sentence into a list of words"""
     for sentence in sentences:
         yield(gensim.utils.simple_preprocess(sentence))
 
-def remove_stopwords(texts):
-    stop_words = nltk.corpus.stopwords.words('english')
+def remove_stopwords(texts, stop_words):
+    "Removes stopwords using a list of stopwords provided"
     return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
 
 def make_bigrams(tokens, bigram):
+    """build the bi-gram models"""
     bigram_mod = gensim.models.phrases.Phraser(bigram)
     return [bigram_mod[doc] for doc in tokens]
+
+trigram = gensim.models.Phrases(bigram[data_words], threshold=100) 
 
 def lemmatization(tokens, allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]):
     """https://spacy.io/api/annotation"""
@@ -39,7 +43,9 @@ def main():
     df = pd.read_csv(file_path)
 
     tokens = list(sent_to_words(list(df["clean"])))
-    tokens_nostops = remove_stopwords(tokens)
+
+    stop_words = nltk.corpus.stopwords.words('english')
+    tokens_nostops = remove_stopwords(tokens, stop_words)
 
     bigrams = gensim.models.Phrases(tokens, min_count=3, threshold=10)
     tokens_bigrams = make_bigrams(tokens_nostops, bigrams)
