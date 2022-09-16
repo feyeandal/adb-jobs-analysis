3249ae5fab045e24ca200f74c34aaa1e550b1cf6
Merl Syrex Chandana <merl.chandana@gmail.com>
2022-09-16 13:21:32 +0530

    updated image_preprocessing script

--- src/image_preprocessing.py
+++ src/image_preprocessing.py
@@ -1,38 +1,56 @@
 from PIL import Image
 import math
+import cv2
+import numpy as np
 
-# import ocr
-# import ocr evaluation steps
-# https://www.youtube.com/watch?v=ADV-AjAXHdc&t=1698s
-# 
-        
-def isLightOrDark(rgbColor):
-    """checks whether a given pixel is light or dark"""
-    [r,g,b] = rgbColor
-    hsp = math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b))
-    if (hsp>127.5):
-        return True
-    else:
-        return False
+def inversion(img):
+    return cv2.bitwise_not(img)
+
+def grayscale(img): #binarization_1
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def blackwhite(gray_image): #binarization_2
+    thresh, im_bw = cv2.threshold(gray_image, 210, 230, cv2.THRESH_BINARY)
+    return im_bw
     
-def binarization(image_path):
-    """converts a color image to black and white"""
-    image = Image.open(image_path)
-    im = image.convert("RGB")
-    pixelMap = im.load()
-    
-    img = Image.new(im.mode, im.size)
-    pixelsNew = img.load()
-    
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            #if light
-            if isLightOrDark(list(pixelMap[i,j])): #function which checks light or dark
-                pixelMap[i,j] = (0,0,0)
-            #if dark
-            else:
-                pixelsNew[i,j] = (254,254,254)
-    return img
+def noise_removal(image): #feed im_bw
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 3)
+    return (image)
+
+def thin_font(image): #eroded_image = thin_font(img)
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+ def thick_font(image): #dilated_image = thick_font(img)
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2,2),np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+ def remove_borders(image): #no_borders = remove_borders(no_noise)
+    contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cntsSorted = sorted(contours, key=lambda x:cv2.contourArea(x))
+    cnt = cntsSorted[-1]
+    x, y, w, h = cv2.boundingRect(cnt)
+    crop = image[y:y+h, x:x+w]
+    return (crop)
+
+def add_borders(image):
+    color = [255, 255, 255]
+    top, bottom, left, right = [150]*4
+    return cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+
 
 
    
