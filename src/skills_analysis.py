802f1c3166270e16e5753a4c9b48598b6dfa0d9d
Merl Syrex Chandana <merl.chandana@gmail.com>
2022-09-19 19:21:57 +0530

    made changes to TM notebook and script

--- src/skills_analysis.py
+++ src/skills_analysis.py
@@ -1,16 +1,15 @@
+# import the relevant packages
 import spacy
 from pprint import pprint
-
 import pandas as pd
 import nltk
-nltk.download('stopwords')
-
 import gensim
 import gensim.corpora as corpora
 from gensim.utils import simple_preprocess
 from gensim.models import CoherenceModel
 
-file_path = "E:/ADB_Project/code/data/pipeline_sample.csv"
+# Download stopwords and spa
+nltk.download('stopwords')
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 
 def sent_to_words(sentences):
@@ -18,16 +17,18 @@ def sent_to_words(sentences):
     for sentence in sentences:
         yield(gensim.utils.simple_preprocess(sentence))
 
-def remove_stopwords(texts, stop_words):
+def remove_stopwords(texts, stop_words=nltk.corpus.stopwords.words('english')):
     "Removes stopwords using a list of stopwords provided"
     return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
 
-def make_bigrams(tokens, bigram):
+def make_bigrams(tokens):
     """build the bi-gram models"""
+    
+    bigram = gensim.models.Phrases(tokens, min_count=3, threshold=10)
+    
     bigram_mod = gensim.models.phrases.Phraser(bigram)
+    
     return [bigram_mod[doc] for doc in tokens]
-
-trigram = gensim.models.Phrases(bigram[data_words], threshold=100) 
 
 def lemmatization(tokens, allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]):
     """https://spacy.io/api/annotation"""
@@ -37,31 +38,34 @@ def lemmatization(tokens, allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]):
         texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
     return texts_out
 
-def main():
+def prep_data(df):
+    """build the bi-gram models"""
 
-    df = pd.read_csv(file_path)
-
-    tokens = list(sent_to_words(list(df["clean"])))
-
-    stop_words = nltk.corpus.stopwords.words('english')
-    tokens_nostops = remove_stopwords(tokens, stop_words)
-
-    bigrams = gensim.models.Phrases(tokens, min_count=3, threshold=10)
-    tokens_bigrams = make_bigrams(tokens_nostops, bigrams)
+    tokens = list(sent_to_words(list(df["clean_text"])))
+    
+    tokens_nostops = remove_stopwords(tokens)
+    
+    tokens_bigrams = make_bigrams(tokens_nostops)
 
     tokens_lemmatized = lemmatization(tokens_bigrams)
+    
+    return tokens_lemmatized
+    
+def build_topic_models(tokens_lemmatized):
+    """topic models"""
+    #Create data formats necessary to build LDA topic models with gensim
+    
+    #Create dictionary
+    id2word = corpora.Dictionary(tokens_lemmatized)
+    
+    # Create Corpus
+    texts = tokens_lemmatized
 
-# Create Dictionary
-id2word = corpora.Dictionary(tokens_lemmatized)
-
-# Create Corpus
-texts = tokens_lemmatized
-
-# Term Document Frequency
-corpus = [id2word.doc2bow(text) for text in texts]
-
-# Build LDA model
-lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+    # Term Document Frequency
+    corpus = [id2word.doc2bow(text) for text in texts]
+    
+    # Build LDA model
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                            id2word=id2word,
                                            num_topics=5, 
                                            random_state=100,
@@ -70,8 +74,22 @@ lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                            passes=10,
                                            alpha='auto',
                                            per_word_topics=True)
-
-#Print the Keyword in the 20 topics
-pprint(lda_model.print_topics())
+    
+    return lda_model
+    
+    
+def main(file_path = "E:/ADB_Project/code/data/pipeline_sample.csv"):
+    
+    df = pd.read_csv(file_path)
+    
+    tokens_lemmatized = prep_data(df)
+    
+    lda_model = build_topic_models(tokens_lemmatized)
+    
+    #Print the Keyword in the 20 topics
+    pprint(lda_model.print_topics())
+    
+if __name__ == "__main__":
+    main()
 
 
