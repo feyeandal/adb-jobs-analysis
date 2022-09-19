07ce3bbd44f3f5c78a31d8731d347390ba756ad4
Vihanga97 <vihanga.dewmini.97@gmail.com>
2022-09-19 18:48:37 +0530

    Edit to return required outputs

--- src/overall_pipeline.py
+++ src/overall_pipeline.py
@@ -4,7 +4,11 @@ import onet_classification
 # Main Function
 def main():
     ocr_pipeline.main()
-    onet_classification.main()
+    matches = onet_classification.main()
+
+    ocr = pd.read_csv('D:/nlp/top_jobs_cs_20_21/part_1/part_1b/p1b.csv')
+    
+    return (ocr, matches)
 
 if __name__ == '__main__':
     main()
