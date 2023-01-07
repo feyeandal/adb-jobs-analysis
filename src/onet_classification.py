8e9378a862118af8ac338ae910e61c1206b70865
merlsyrex <merl.chandana@gmail.com>
2023-01-07 13:52:45 +0530

    testing_and_fixes

--- src/onet_classification.py
+++ src/onet_classification.py
@@ -260,7 +260,8 @@ def main(data_path, occ_path, alt_path, tech_path, ocr_output_path, lockdown_dat
     logging.info('Reading onet data')
     onet_data = read_onet_data(occ_path, alt_path, tech_path)
 
-    print(type(onet_data))
+    print(type(onet_data.shape))
+    print(onet_data.head())
 
     logging.info('Reading topjobs data')
     sample = read_topjobs_data(data_path)
@@ -270,6 +271,9 @@ def main(data_path, occ_path, alt_path, tech_path, ocr_output_path, lockdown_dat
 
     logging.info('Appending ocr output')
     sample = append_ocr_output(ocr_output_path, sample)
+
+    print(sample.shape)
+    print(sample.head())
 
     logging.info('Preparing sample')
     sample = prepare_sample(sample, lockdown_date_range)
